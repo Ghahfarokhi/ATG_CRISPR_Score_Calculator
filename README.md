2445b514883462fe7de88c5023717af4d660146a
Amir Taheri-Ghahfarokhi <Amir.Taheri.Ghahfarokhi@gmail.com>
2023-12-26 22:03:05 +0100

    Added the "pairList" run mode!

--- README.md
+++ README.md
@@ -2,31 +2,51 @@
  CFD (Cutting Frequency Determination) score for a given CRISPR guide RNA (crRNA) and DNA sequence is used to estimate the likelihood of a particular guide RNA inducing a double-strand break at a specific genomic targeted by *Streptococcus pyogenes* Cas9 (SpyCas9).
 
  ### Command line arguments:
-      
-    arg_1: cfdScores_file. 
-    arg_2: crRNA sequence (case-insensitive letters: A,T/U,C,G,-), with PAM.
-    arg_3: DNA sequence (case-insensitive letters: A,T,C,G,-), with PAM.
+
+ You can run this program in two modes: `singlePair` and `pairList`.
+
+ ### Usage: 
+**singlePair**: `./cfdScoresCalculator singlePair <cfdScoresFilePath> <crRNASequence> <DNASequence> `
+
+Example:
+```
+./cfdScoresCalculator singlePair data/CFD_Scores.txt ATCGATGCTGATGCTAGATAAGG ACCGATGCTGATGCTAGATAAGG 
+
+output:
+0.857143
+```
+
+**pairList**: `cfdScoresCalculator pairList <cfdScoresFilePath> <pairListFilePath>`
+Example: 
+
+```
+./cfdScoresCalculator pairList data/CFD_Scores.txt data/crRNA_DNA_Pair_Examples.txt
+
+output:
+crRNA   DNA     CFD
+ATCGATGCTGATGCTAGATAAGG ATCGATGCTGATGCTAGATAAGG 1
+ATCGATGCTGATGCTAGATAAGG AcCGATGCTGATGCTAGATAAGG 0.857143
+ATCGATGCTGATGCTAGATAAGG ATCGATGCTGATGCTAGATtAGG 0.6
+ATCGATGCTGATGCTAGATAAGG ATCGATGCTGATGCTAGATAAGa 0.0694444
+ATCGATGCTGATGCTAGATAAGG A-CGATGCTGATGCTAGATAAGG 0.953125
+AT-CGATGCTGATGCTAGATAAGG        ATaCGATGCTGATGCTAGATAAGG        0.727273
+```
+
 
 Note: crRNA and DNA squences must be at least 23 nucleotides or longer and end with their PAMs. 
 
 ### Requirements: 
- - **CFD_Scores.txt**: A valid cfdScoreLine in the CFD_Scores.txt file is composed of a Label as string and a decimal number as double, in a tab-delimited format (e.g.,: `rU:dG_2   0.857143`). Labels should also include the CFD scores for PAMs. See `data/CFD_Scores.txt` file for details. 
+ - **CFD_Scores.txt**: A valid cfdScoreLine in the `CFD_Scores.txt` file is composed of a Label as string and a decimal number as double, in a tab-delimited format (e.g.,: `rU:dG_2   0.857143`). Labels should also include the CFD scores for PAMs. See `data/CFD_Scores.txt` file for details. 
+- **crRNA** sequence: case-insensitive letters: A,T/U,C,G,-, 23 nucleotides or longer, ending with PAM. 
+- **DNA** sequence: case-insensitive letters: A,T,C,G,-, 23 nucleotides or longer (but must have equal length to crRNA), ending with PAM. 
 
 ### Installation
 ```
 git clone https://github.com/Ghahfarokhi/CFD_Scores_Calculator.git
 cd CFD_Scores_Calculator
 make cfdScoresCalculator
+./cfdScoresCalculator -h
 ```
-
-### Usage
-
-**Example usage**:
-```
-./cfdScoresCalculator data/CFD_Scores.txt ATCGATGCTGATGCTAGATAAGG ACCGATGCTGATGCTAGATAAGG 
-```
-
-**Output**: 0.857143
 
 **Reference**: 
 - Doench, J., et al. (2016) Nat Biotechnol 34, 184–191.
@@ -34,4 +54,4 @@ make cfdScoresCalculator
 
 **Developer**: Amir.Taheri.Ghahfarokhi@Gmail.com 
 
-**Initial release**: 2023-12-25
+**Initial release**: 2023-12-26
--- cfdScoresCalculator.cpp
+++ cfdScoresCalculator.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <map>
 
-// Function to get the reverse complement of a DNA sequence
 std::string revComp(const std::string& DNASequence) {
     std::string complement;
     for (char base : DNASequence) {
@@ -28,40 +27,11 @@ std::string revComp(const std::string& DNASequence) {
     return complement;
 }
 
-void printHelp(){
-    const char *helpText = "\nThis program takes the following command line arguments:\n\
-      arg_1: cfdScores_file. \n\
-      arg_2: crRNA sequence (case-insensitive letters: A,T/U,C,G,-), with PAM.\n\
-      arg_3: DNA sequence (case-insensitive letters: A,T,C,G,-), with PAM.\n\n\
-      Note: crRNA and DNA squences must be at least 23 nucleotides or longer and end with their PAMs. \n\n\
-    Usage example: \n\
-    cfdScores CFD_Scores.txt ATCGATGCTGATGCTAGATAAGG ACCGATGCTGATGCTAGATAAGG \n\n\
-    A valid cfdScoreLine in the CFD_Scores.txt file is composed of a Label as string and a decimal number as double, tab-delimited. \n\
-    Example: rA:dA_10	0.882352941 \n\
-    Labels should also include the CFD scores for PAMs. \n\n\
-    CFD Score Reference: Doench, J., et al. (2016) Nat Biotechnol 34, 184–191. \n\n\
-    Developer: Amir.Taheri.Ghahfarokhi@Gmail.com \n\
-    Release: 2023-12-25\n\n";
-    std::cout << helpText << std::endl;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc == 2 && std::string(argv[1]) == "-h") {
-        printHelp();
-        return 0;
-    }
-    
-    if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <cfdScores_file> <crRNA_sequence> <DNA_sequence>" << std::endl;
-        return 1;
-    }
-
-    std::string cfdScoresFilePath(argv[1]);
+std::map<std::string, double> cfdScoresFunc(std::string cfdScoresFilePath){
+    // Read the cfdScoresFile that will be used in both singlePair and pairList modes.
     std::ifstream cfdScoresFile(cfdScoresFilePath);
-
     if (!cfdScoresFile.is_open()) {
         std::cerr << "Error opening file: " << cfdScoresFilePath << std::endl;
-        return 1;
     }
 
     std::map<std::string, double> cfdScores;
@@ -83,9 +53,21 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Get crRNA and DNA sequences from command cfdScoreLine arguments
-    std::string crRNASequence(argv[2]);
-    std::string DNASequence(argv[3]);
+    return cfdScores;
+}
+
+double singlePairFunc (std::map<std::string, double> cfdScores, std::string crRNASequence, std::string DNASequence) {
+    // Replace "T" with "U" in crRNA and transform both DNA and crRNA to uppercase
+    for (char &ch : crRNASequence) {
+        ch = std::toupper(ch);
+        if (ch == 'T') {
+            ch = 'U';
+        }
+    }
+
+    for (char &ch : DNASequence) {
+        ch = std::toupper(ch);
+    }
 
     // Extract the rightmost 2 characters of the DNA as PAM
     std::string PAM = DNASequence.substr(DNASequence.length() - 2);
@@ -99,22 +81,10 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Replace "T" with "U" in crRNA and transform both DNA and crRNA to uppercase
-    for (char &ch : crRNASequence) {
-        if (ch == 'T') {
-            ch = 'U';
-        }
-        ch = std::toupper(ch);
-    }
+    double cfdProduct = 1.0;
+    cfdProduct *= cfdScores[PAM];
 
-    for (char &ch : DNASequence) {
-        ch = std::toupper(ch);
-    }
-
-    double product = 1.0;
-    product *= cfdScores[PAM];
-
-    if (product > 0) {
+    if (cfdProduct > 0) {
         // Perform pairwise comparison and product calculation
         for (int i = 0; i < crRNASequence.length(); ++i) {
             // Construct label for each pair with underscore and position
@@ -124,12 +94,99 @@ int main(int argc, char *argv[]) {
             double cfdScore = cfdScores[label];
 
             // Multiply the product
-            product *= cfdScore;
+            cfdProduct *= cfdScore;
         }
     }
-    
-    // Print the final CFD score
-    std::cout << product << std::endl;
 
-    return 0;
+    return cfdProduct;
+};
+
+void printHelp(){
+    const char *helpText = "\nCutting Frequency Determination (CFD) Calculator\n\
+    Usage: \n\
+        cfdScoresCalculator -h \n\
+        OR\n\
+        cfdScoresCalculator singlePair <cfdScoresFilePath> <crRNASequence> <DNASequence> \n\
+        OR\n\
+        cfdScoresCalculator pairList <cfdScoresFilePath> <pairListFilePath>\n\n\
+    Examples:\n\
+        ./cfdScoresCalculator singlrPair data/CFD_Scores.txt ATCGATGCTGATGCTAGATAAGG ACCGATGCTGATGCTAGATAAGG \n\
+        ./cfdScoresCalculator pairList data/CFD_Scores.txt data/crRNA_DNA_Pair_Examples.txt \n\n\
+    Note: crRNA and DNA squences provided in singlePair and pairList run modes must be at least 23 nucleotides or longer and end with their PAMs.\n\n\
+    Reference: Doench, J., et al. (2016) Nat Biotechnol 34, 184–191. \n\n\
+    Developer: Amir.Taheri.Ghahfarokhi@Gmail.com \n\
+    Release: 2023-12-26\n\n";
+    std::cout << helpText << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    // Define variables that will be used to store command line arguments
+    std::string runMode, cfdScoresFilePath, crRNASequence, DNASequence, pairListFilePath;
+    double cfdProduct = 1.0;
+
+    // Check the provided arguments
+    if (argc == 2 && std::string(argv[1]) == "-h") {
+        printHelp();
+        return 0;
+    } else if (argc == 5 && std::string(argv[1]) == "singlePair") {
+        runMode = std::string(argv[1]);
+        cfdScoresFilePath = std::string(argv[2]);
+        crRNASequence = std::string(argv[3]);
+        DNASequence = std::string(argv[4]);
+    } else if (argc == 4 && std::string(argv[1]) == "pairList") {
+        runMode = std::string(argv[1]);
+        cfdScoresFilePath = std::string(argv[2]);
+        pairListFilePath = std::string(argv[3]);
+    } else {
+        printHelp();
+        return 1;
+    }
+
+    // Read cfdScores from cfdScoresFilePath
+    std::map<std::string, double> cfdScores;
+    cfdScores = cfdScoresFunc(cfdScoresFilePath);
+
+    if (runMode == "singlePair") {
+
+        cfdProduct = singlePairFunc(cfdScores, crRNASequence, DNASequence);
+        // Print the final CFD score
+        std::cout << cfdProduct << std::endl;
+
+        return 0;
+
+    } else if (runMode == "pairList") {
+        
+        // Read pairList from pairListFilePath
+        std::ifstream pairListFile(pairListFilePath);
+        if (!pairListFile.is_open()) {
+            std::cerr << "Error opening file: " << pairListFilePath << std::endl;
+            return 1;
+        }
+
+        // Process each line in the pairList file
+        std::string pairLine;
+        bool isFirstLine = true; 
+        while (std::getline(pairListFile, pairLine)) {
+            std::istringstream iss(pairLine);
+            // Skip the header line
+            if (isFirstLine) {
+                isFirstLine = false;
+                std::cout << pairLine << "\t" << "CFD" << std::endl;
+                continue;
+            }
+                
+            // Split the line into crRNA and DNA
+            if (std::getline(iss, crRNASequence, '\t') && std::getline(iss, DNASequence, '\t')) {
+                cfdProduct = singlePairFunc(cfdScores, crRNASequence, DNASequence);
+                std::cout << crRNASequence << "\t" << DNASequence << "\t" << cfdProduct << std::endl;
+            } else {
+                std::string errorText;
+                errorText = "Error parsing pairLine: " + pairLine + " in " + pairListFilePath;
+                std::cerr << errorText << std::endl;
+                return 1;
+            }
+        }
+
+        return 0;
+    }
 }
